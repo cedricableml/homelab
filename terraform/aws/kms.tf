206248562adc68a6f17d1dc2e0dfe0314b2d0dae
Dragosh <39906742+0dragosh@users.noreply.github.com>
2021-10-16 18:39:56 +0300

    Re-enable vault access key

--- terraform/aws/kms.tf
+++ terraform/aws/kms.tf
@@ -1,38 +1,38 @@
-#resource "aws_kms_key" "homelab" {
-#  description = "homelab KMS"
-#}
-#
-#resource "aws_iam_user" "vault" {
-#  name = "vault"
-#
-#  tags = {
-#    terraform = "true"
-#  }
-#}
-#
-#resource "aws_iam_access_key" "vault" {
-#  user = aws_iam_user.vault.name
-#}
-#
-#data "aws_iam_policy_document" "vaultkms" {
-#  statement {
-#    sid = "1"
-#
-#    actions = [
-#      "kms:DescribeKey",
-#      "kms:Encrypt",
-#      "kms:Decrypt"
-#    ]
-#
-#    resources = [
-#      aws_kms_key.homelab.arn,
-#    ]
-#  }
-#}
-#
-#resource "aws_iam_user_policy" "vault_kms" {
-#  name = "vaultkms"
-#  user = aws_iam_user.vault.name
-#
-#  policy = data.aws_iam_policy_document.vaultkms.json
-#}
+resource "aws_kms_key" "homelab" {
+  description = "homelab KMS"
+}
+
+resource "aws_iam_user" "vault" {
+  name = "vault"
+
+  tags = {
+    terraform = "true"
+  }
+}
+
+resource "aws_iam_access_key" "vault" {
+  user = aws_iam_user.vault.name
+}
+
+data "aws_iam_policy_document" "vaultkms" {
+  statement {
+    sid = "1"
+
+    actions = [
+      "kms:DescribeKey",
+      "kms:Encrypt",
+      "kms:Decrypt"
+    ]
+
+    resources = [
+      aws_kms_key.homelab.arn,
+    ]
+  }
+}
+
+resource "aws_iam_user_policy" "vault_kms" {
+  name = "vaultkms"
+  user = aws_iam_user.vault.name
+
+  policy = data.aws_iam_policy_document.vaultkms.json
+}
