7b041384778e512b2084d110f6be3a52177afaa5
Dragosh <39906742+0dragosh@users.noreply.github.com>
2021-11-13 12:34:09 +0200

    rotate cf tunnel

--- terraform/cloudflare/cloudflare_argo_tunnel.tf
+++ terraform/cloudflare/cloudflare_argo_tunnel.tf
@@ -1,9 +1,19 @@
 resource "random_id" "secret-1" {
-  byte_length = 32
+  byte_length = 40
 }
 
 resource "cloudflare_argo_tunnel" "tunnel1" {
   name       = "cloudflaredtunnel1"
   account_id = var.cloudflare_account_id
   secret     = random_id.secret-1.b64_std
+}
+
+resource "random_id" "secret-2" {
+  byte_length = 40
+}
+
+resource "cloudflare_argo_tunnel" "tunnel2" {
+  name       = "cloudflaredtunnel2"
+  account_id = var.cloudflare_account_id
+  secret     = random_id.secret-2.b64_std
 }
