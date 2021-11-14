13c3ad8641cdf6ea4cd5f4a07ff4cd1deefdcd04
Dragosh <39906742+0dragosh@users.noreply.github.com>
2021-11-14 21:14:55 +0200

    clean up tf

--- terraform/cloudflare/cloudflare_applications.tf
+++ terraform/cloudflare/cloudflare_applications.tf
@@ -162,26 +162,10 @@ locals {
   ]
 }
 
-
 resource "cloudflare_access_service_token" "token" {
   for_each   = {for app in local.apps : app.subdomain => app}
   account_id = var.cloudflare_account_id
   name       = each.value.subdomain
-}
-
-resource "cloudflare_access_policy" "token" {
-  for_each = {for app in local.apps : app.subdomain => app}
-
-  zone_id        = var.zone_id
-  application_id = cloudflare_access_application.app_casa[each.value.subdomain].id
-
-  name       = "allow any service token"
-  precedence = "5"
-  decision   = "non_identity"
-
-  include {
-    service_token = [cloudflare_access_service_token.token[each.value.subdomain].id]
-  }
 }
 
 resource "cloudflare_access_application" "app_casa" {
@@ -193,35 +177,4 @@ resource "cloudflare_access_application" "app_casa" {
   type                      = each.value.type
   session_duration          = "240h"
   auto_redirect_to_identity = true
-}
-
-resource "cloudflare_access_policy" "gsuite_casa" {
-  for_each = {for app in local.apps : app.subdomain => app}
-
-  zone_id        = var.zone_id_casa
-  application_id = cloudflare_access_application.app_casa[each.value.subdomain].id
-
-  name       = "allow ${var.email_domain}"
-  precedence = "10"
-  decision   = "allow"
-
-  dynamic "include" {
-    for_each = each.value.gsuite == true ? [1] : []
-    content {
-      email_domain = [var.email_domain]
-    }
-  }
-}
-
-resource "cloudflare_access_policy" "everyone_calibre-web" {
-  zone_id        = var.zone_id_casa
-  application_id = cloudflare_access_application.app_casa["calibre-web"].id
-
-  name       = "allow group with pin"
-  precedence = "20"
-  decision   = "allow"
-
-  include {
-    group = ["546e1fbc-2441-4852-83a0-a664c45cff3d"] # everyone
-  }
 }
