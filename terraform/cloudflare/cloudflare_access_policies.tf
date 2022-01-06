195c6f41d20764d5ac37de7f1a91678950ac3d08
Dragosh <39906742+0dragosh@users.noreply.github.com>
2022-01-06 13:28:04 +0200

    add new group cfa policy

--- terraform/cloudflare/cloudflare_access_policies.tf
+++ terraform/cloudflare/cloudflare_access_policies.tf
@@ -31,18 +31,18 @@ resource "cloudflare_access_policy" "gsuite_casa" {
   }
 }
 
-#resource "cloudflare_access_policy" "everyone_calibre-web" {
-#  zone_id        = var.zone_id_casa
-#  application_id = cloudflare_access_application.app_casa["calibre-web"].id
-#
-#  name       = "allow group with pin"
-#  precedence = "20"
-#  decision   = "allow"
-#
-#  include {
-#    group = ["546e1fbc-2441-4852-83a0-a664c45cff3d"] # everyone
-#  }
-#}
+resource "cloudflare_access_policy" "everyone_calibre-web" {
+  zone_id        = var.zone_id_casa
+  application_id = cloudflare_access_application.app_casa["calibre-web"].id
+
+  name       = "allow group with pin"
+  precedence = "20"
+  decision   = "allow"
+
+  include {
+    group = ["bbe2db0c-ab33-459b-b6e5-adaa2dc4f7be"] # fam
+  }
+}
 #
 #resource "cloudflare_access_policy" "everyone_requests" {
 #  zone_id        = var.zone_id_casa
