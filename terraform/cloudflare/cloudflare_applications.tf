b1647bd61d396e35544f16a53cfaf16ba91785fe
Dragosh <39906742+0dragosh@users.noreply.github.com>
2022-01-23 09:33:51 +0200

    add minio to cfa

--- terraform/cloudflare/cloudflare_applications.tf
+++ terraform/cloudflare/cloudflare_applications.tf
@@ -190,6 +190,11 @@ locals {
       type      = "self_hosted"
       gsuite    = true
     },
+    {
+      subdomain = "minio"
+      type      = "self_hosted"
+      gsuite    = true
+    },
   ]
   apps_secondary = [
     {
