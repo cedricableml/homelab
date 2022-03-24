87d069accd88ffc89a7b71789c3dbb634e1ec6a1
Dragosh <39906742+0dragosh@users.noreply.github.com>
2022-03-24 15:59:55 +0200

    fix tf

--- terraform/aws/state.tf
+++ terraform/aws/state.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    # organization from TF_CLI_ARGS_init="-backend-config='organization=$TFE_ORG'"
+    organization = "cloudpush"
     workspaces {
       name = "homelab-aws"
     }
