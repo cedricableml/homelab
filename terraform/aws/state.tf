f4f92b078730c5db5c4ded1a49e68f8fe29b802f
Dragos Schebesch <dragos@cloudpush.tech>
2022-04-22 17:35:30 +0300

    cleanup workflows

--- terraform/aws/state.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "remote" {
-    organization = "cloudpush"
-    workspaces {
-      name = "homelab-aws"
-    }
-  }
-}
--- terraform/aws/vars.tf
+++
@@ -1,5 +0,0 @@
-variable "region" {
-  default = "eu-central-1"
-}
-
-variable "account_id" {}
