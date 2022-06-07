5015baed6a269bab409294de5a2a2b8f09019197
Dragosh <39906742+0dragosh@users.noreply.github.com>
2022-06-07 18:19:57 +0300

    rm flux bootstrap via tf

--- terraform/talos/providers.tf
+++ terraform/talos/providers.tf
@@ -4,18 +4,3 @@ provider "vsphere" {
   vsphere_server = var.vsphere_address
   allow_unverified_ssl = true
 }
-
-provider "flux" {
-  # Configuration options
-}
-
-#provider "kubectl" {}
-#
-#provider "kubernetes" {
-#  config_path = "~/.kube/config"
-#}
-
-provider "github" {
-  owner = var.github_owner
-  token = var.github_token
-}
